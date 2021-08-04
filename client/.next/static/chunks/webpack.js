634188d5ff5ea52357f15cc02bb15153c8bc9a59
daeho <dlaeogh8572@gmail.com>
2021-08-04 22:31:12 +0900

    add: server api

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "65a9d7f64b21b2537634"; }
+/******/ 		__webpack_require__.h = function() { return "844770fd0a3256aabe6b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
