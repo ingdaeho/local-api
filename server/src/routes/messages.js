82f07b1550b55865111ce4bdb240193b8a4675e7
daeho <dlaeogh8572@gmail.com>
2021-08-07 17:31:32 +0900

    mod: lowdb

--- server/src/routes/messages.js
+++ server/src/routes/messages.js
@@ -1,8 +1,11 @@
-import { readDB, writeDB } from "../dbController.js";
+import db from "../dbController.js";
 import { v4 } from "uuid";
 
-const getMsgs = () => readDB("messages");
-const setMsgs = (data) => writeDB("messages", data);
+const getMsgs = () => {
+  db.read();
+  db.data = db.data || { messages: [] };
+  return db.data.messages;
+};
 
 const messagesRoute = [
   {
@@ -44,8 +47,8 @@ const messagesRoute = [
           userId: body.userId,
           timestamp: Date.now(),
         };
-        msgs.unshift(newMsg);
-        setMsgs(msgs);
+        db.data.messages.unshift(newMsg);
+        db.write();
         res.send(newMsg);
       } catch (err) {
         res.status(500).send({ error: err });
@@ -65,8 +68,8 @@ const messagesRoute = [
           throw "사용자가 다릅니다.";
 
         const newMsg = { ...msgs[targetIndex], text: body.text };
-        msgs.splice(targetIndex, 1, newMsg);
-        setMsgs(msgs);
+        db.data.messages.splice(targetIndex, 1, newMsg);
+        db.write();
         res.send(newMsg);
       } catch (err) {
         res.status(500).send({ error: err });
@@ -84,8 +87,8 @@ const messagesRoute = [
         if (targetIndex < 0) throw "메세지가 없습니다.";
         if (msgs[targetIndex].userId !== userId) throw "사용자가 다릅니다.";
 
-        msgs.splice(targetIndex, 1);
-        setMsgs(msgs);
+        db.data.messages.splice(targetIndex, 1);
+        db.write();
         res.send(id);
       } catch (err) {
         res.status(500).send({ error: err });
